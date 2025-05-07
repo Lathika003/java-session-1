a282ebd92e09cbc82cbb4777224b8ec8118431f6
Lathika Nipun <145969120+Lathika003@users.noreply.github.com>
2025-05-07 18:19:07 +0530

    Add files via upload

---
+++ P2.java
@@ -0,0 +1,10 @@
+import java.io.*;   //this is how we import packages
+
+class P2{   //class name should be start with capital letter
+
+	public static void main(String[] args){
+		System.out.println("The first argument is "+args[0]);
+		System.out.println("The second argument is "+args[1]);
+		System.out.print("The third argument is "+args[2]);
+	}
+}
---
+++ first.java
@@ -0,0 +1,14 @@
+//this is a comment
+
+
+import java.io.*;   //this is how import packages
+
+class Myfirst{   //class name should be start with capital letter
+
+	public static void main(String[] args){
+		System.out.print("Hello");
+		System.out.print("This is IT1214 practical session 1");
+		System.out.println("This prints in a new Line");
+		System.out.println("This is a new Line");
+	}
+}
